56732d2551767f822831fdef4eafa9585737cd05
hn-seoai <hn@seo.ai>
2024-06-28 14:30:39 +0200

    Convert NullType to nullable property

--- tests/TypeToSchemaTransformerTest.php
+++ tests/TypeToSchemaTransformerTest.php
@@ -161,7 +161,8 @@ it('infers date column directly referenced in json as date-time', function () {
     ]);
 
     expect($components->getSchema(InferTypesTest_JsonResourceWithCarbonAttribute::class)->toArray()['properties']['created_at'])->toBe([
-        'type' => ['string', 'null'],
+        'type' => 'string',
+        'nullable' => true,
         'format' => 'date-time',
     ]);
 });
